d8ac8ddbcb11c1352e09a2a1c348d0f29035f4a1
lcdamy <zudanga@gmail.com>
2025-03-07 23:02:07 +0200

    Add Dashboard, Login, and Register components; implement Navbar and theme configuration

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+
+const navItems = ['Home', 'Login', 'Register'];
+
+function Navbar() {
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+           <img src="/Bit.ly_Logo.svg.png" alt="My App Logo" style={{ height: '40px', marginRight: '10px' }} />
+
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+      </nav>
+
+    </Box>
+  );
+}
+
+
+export default Navbar;
---
+++ src/theme/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#031f39',
+    },
+    secondary: {
+      main: '#ffffff',
+    },
+  },
+});
+
+export default theme;
