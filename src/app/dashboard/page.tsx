3df444a8d51dd64edb74cfe697a0444b42f5fb1a
lcdamy <zudanga@gmail.com>
2025-03-09 01:10:55 +0200

    Refactor Dashboard component to replace table with list for displaying shortened links

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,69 +1,46 @@
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { Box, Button, Container } from '@mui/material';
-import VisibilityIcon from '@mui/icons-material/Visibility';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import { Box, Button, Container, IconButton } from '@mui/material';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
-function createData(id: string, calories: string, fat: string) {
-  return { id, calories, fat };
-}
 
-const rows = [
-  createData("001", "http://localhost:3005/dashboard", "http://localhost:3005/dashboard"),
-  createData("001", "http://localhost:3005/dashboard", "http://localhost:3005/dashboard"),
-  createData("001", "http://localhost:3005/dashboard", "http://localhost:3005/dashboard"),
-
-];
 
 export default function Dashboard() {
   return (
     <Container>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <h1>Dashboard</h1>
+        <h1>Shorten links</h1>
         <Box>
           <Button href="/" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}> Shorten URL </Button>
         </Box>
       </Box>
-      <TableContainer component={Paper} sx={{ marginTop: '20px' }}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell align="left">Shorten Version</TableCell>
-              <TableCell align="left">Orginal Version</TableCell>
-              <TableCell align="right">Action</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+          {[0, 1, 2, 3].map((value) => {
+            const labelId = `checkbox-list-label-${value}`;
+            return (
+              <ListItem
+                key={value}
+                secondaryAction={
+                  <IconButton edge="end" aria-label="comments">
+                    <ContentCopyIcon />
+                  </IconButton>
+                }
+                disablePadding
               >
-                <TableCell component="th" scope="row">
-                  {row.id}
-                </TableCell>
-                <TableCell align="left">{row.calories}</TableCell>
-                <TableCell align="left">{row.fat}</TableCell>
-                <TableCell align="left">
-                  <Box sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
-                    <a href={`/dashboard/${row.id}`} style={{ display: 'flex', alignItems: 'center' }}>
-                      <VisibilityIcon sx={{ cursor: 'pointer', ':hover': { color: '#0b1736' } }} />
-                    </a>
-                    <ContentCopyIcon sx={{ cursor: 'pointer', ':hover': { textDecoration: 'underline' } }} />
-                  </Box>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+                <ListItemButton role={undefined} dense component="a" href={`dashboard/${value.toString()}`}>
+                  <ListItemText id={labelId} primary={`Line item ${value + 1} `} />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </List>
+      </Box>
+
     </Container>
   );
 }
